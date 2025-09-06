7bc034316f256f608f25c7ab798abd189ac9696d
cheeky-2 <jjuing0@gmail.com>
2025-09-06 17:41:51 +0900

    빌드 실패 후 다시 시도

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { tick } from "./store";
-import Main from "./components/Main";
+import Main from "./components/MainPage";
 
 function App() {
   const dispatch = useDispatch();
