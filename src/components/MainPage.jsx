234edfdad00dae5ad94f378fe4318d29a27db922
cheeky-2 <jjuing0@gmail.com>
2025-09-07 13:43:16 +0900

    MainPageCSS분리

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { addScore } from "../store";
 import MenuAll from "./interface/MenuAll";
 import mainItem from '../assets/images/main_item.png';
+import '../MainPage.css';
 /**
  * useSelector: 스토어의 특정 상태를 "구독"해서 가져옴.
  * - state: 스토어 전체 루트 상태 { game: { score, ... } }
@@ -12,9 +13,9 @@ function Main() {
     const dispatch = useDispatch();
 
     return (
-    <div>
-        <h1 style={{textAlign:"center", fontSize:'1.75rem', marginTop:'20px', fontWeight:'600'}}>달달구리: {score}</h1>
-        <button onClick={() => dispatch(addScore())} style={{width:'400px', margin:'40px auto 60px', display:"block"}}><img src={mainItem} alt="" style={{width:'100%'}} /></button>
+    <div className="main_page">
+        <h1>달달구리: {score}</h1>
+        <button onClick={() => dispatch(addScore())}><img src={mainItem} alt="" /></button>
         <MenuAll />
     </div>
     );
